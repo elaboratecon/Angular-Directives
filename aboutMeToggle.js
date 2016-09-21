3b6b3fdfa59e3a1e8ff1818a6d81a7d8c4604085
Jason Conover <JasonConover@Jasons-MacBook-Pro.local>
2016-09-21 09:23:47 -0700

    AboutMe toggle attribute animates homepage

---
+++ aboutMeToggle.js
@@ -0,0 +1,33 @@
+app.directive('aboutMeToggle', function (){
+  return {
+  	restrict: 'A',
+    controller: 'AboutMe',
+    link: function (scope, elem, attrs){
+
+      var initText = angular.element('#initText');
+      var aboutMe = angular.element('#about_me');
+      var moreText = angular.element('#moreText');
+      
+      var toggleSplash = function(){
+        if(scope.myValue==false){
+          initText.fadeOut(150, function(){
+            aboutMe.animate({marginLeft: '-=100%'}, 900, function(){
+              moreText.fadeIn(150);
+            });
+          });
+          scope.aboutMeButton = "Back"
+          scope.myValue = true;
+        } else {
+          moreText.fadeOut(150, function(){
+            aboutMe.animate({marginLeft: '+=100%'}, 900, function(){
+              initText.fadeIn(150);
+            });
+          });
+          scope.aboutMeButton = "A little more about me..."
+          scope.myValue = false;
+        }
+      };
+      elem.bind('click', toggleSplash);
+    }
+  };
+});
